56bb00ad90e4019c0f60841126b3fb589aa331b7
satvika39 <satvikavelagala39@gmail.com>
2022-12-05 13:37:44 +0530

    first commit

---
+++ parkinglot.py
@@ -0,0 +1,11 @@
+#CAMEL casing means the way we write in the this form
+noOfCars=0
+noOfBikes=0
+totalPayment=0
+noOfCars=int(input("No of cars"))
+noOfBikes=int(input("No of Bikes"))
+totalPayment=noOfBikes*20+noOfCars*40
+print("total payment would be",totalPayment)
+
+
+
---
+++ product.py
@@ -0,0 +1,11 @@
+product1=int(input("enter product1"))
+product2=int(input("enter product2"))
+if (product1<=0 or product2<=0):
+    print("enter a positive number")
+#product2=int(input("enter product2"))
+#product3=int(input("enter product3"))
+#totalCount=product1*40+product2*50+product3*60
+#print("user need to pay:",totalCount)
+
+#if(product1<=0):
+ #   print("enter apositive number")
